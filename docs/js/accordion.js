2448a6bf116c45df9c5a12544dacfbd638bcb06c
Vlad <vladyslavmyronov@gmail.com>
2020-02-02 01:45:59 +0200

    almost done  making accordion

--- docs/js/accordion.js
+++ docs/js/accordion.js
@@ -1,6 +1,20 @@
 $("#accordion").on("hide.bs.collapse show.bs.collapse", e => {
-  $(e.target).
-  prev().
-  find("i:last-child").
-  toggleClass("fa-minus fa-plus");
+  $(e.target)
+    .prev()
+    .find("i:last-child")
+    .toggleClass("fa-minus fa-plus");
+});
+
+$("#accordion2").on("hide.bs.collapse show.bs.collapse", e => {
+  $(e.target)
+    .prev()
+    .find("i:last-child")
+    .toggleClass("fa-minus fa-plus");
+});
+
+$("#accordion3").on("hide.bs.collapse show.bs.collapse", e => {
+  $(e.target)
+    .prev()
+    .find("i:last-child")
+    .toggleClass("fa-minus fa-plus");
 });
